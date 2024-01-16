cbed7f2c5554aa0b662b24780742a13d89f520d0
Marcelo Dev <107871318+SenhorDosSonhos1@users.noreply.github.com>
2024-01-16 11:33:38 -0300

    Update 1app.py

--- 1app.py
+++ 1app.py
@@ -14,7 +14,7 @@ msg['To'] = 'marcelosamu2002@gmail.com'
 msg.set_content('Olá, tudo bem?!\nJá fez sua verificação de segurança hoje?\n recomendamos fazer uma verificação todos os dias\n Uma falha de segurança é uma grande perigo!\n sempre lembre de anotar as vunerabilidades corrigidas na documentação!\n https://github.com/Lacrei/lacrei-institucional/actions/workflows/owasp_zap.yml\nhttps://www.notion.so/lacrei/f4b829cba43c414cbb8e0e491d20e623?v=40912c3f99ef4e9fb9d4a05b6d8ce98e  \nATENCIOSAMENTE: SCAN_OWASP')
 
 # Usar a porta 587 com STARTTLS
-with smtplib.SMTP('smtp-mail.outlook.com', 587) as smtp:
+with smtplib.SMTP('smtp.gmail.com', 465) as smtp:
     # Iniciar o processo de STARTTLS
     smtp.starttls()
 
