8cbf9a73c8034c872f6dd576de94a611e7c356ed
Marcelo Dev <107871318+SenhorDosSonhos1@users.noreply.github.com>
2024-01-16 09:26:29 -0300

    Update 1app.py

--- 1app.py
+++ 1app.py
@@ -3,14 +3,14 @@ import smtplib
 from email.message import EmailMessage
 
 #Seu email de envio, lembre de configurar a secret_key do action com a senha do email definido abaixo
-EMAIL_ADDRESS = "scan_lacrei@outlook.com"
+EMAIL_ADDRESS = "torcidas2002@gmail.com"
 EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
 
 # Criando email
 msg = EmailMessage()
 msg['Subject'] = 'DAST LACREI'
 msg['From'] = EMAIL_ADDRESS 
-msg['To'] = 'teste1brvs@gmail.com'
+msg['To'] = 'marcelosamu2002@gmail.com'
 msg.set_content('Olá, tudo bem?!\nJá fez sua verificação de segurança hoje?\n recomendamos fazer uma verificação todos os dias\n Uma falha de segurança é uma grande perigo!\n sempre lembre de anotar as vunerabilidades corrigidas na documentação!\n https://github.com/Lacrei/lacrei-institucional/actions/workflows/owasp_zap.yml\nhttps://www.notion.so/lacrei/f4b829cba43c414cbb8e0e491d20e623?v=40912c3f99ef4e9fb9d4a05b6d8ce98e  \nATENCIOSAMENTE: SCAN_OWASP')
 
 # Usar a porta 587 com STARTTLS
